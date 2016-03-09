67d63d0b632e9d60f42a83efb0a19c8f64add02f
Jean Rigotti <jean.michael.rigotti@sap.com>
2016-03-09 16:30:25 -0300

    Add own time data

--- Parser.hs
+++ Parser.hs
@@ -1,27 +1,20 @@
-import Data.Time
-import Data.Attoparsec.Char8
+import Text.ParserCombinators.ReadP
+import Text.Printf
 
-data LogEntry = 
-    LogEntry { seq     :: Int
-             , start   :: LocalTime
-             , end     :: LocalTime
-             , message :: String 
-             } deriving Show
+-- Creating an specific dateformat
+-- used in srt files
+data SrtTime = SrtTime { hours   :: Int
+                       , minutes :: Int
+                       , seconds :: Int
+                       , milli   :: Int }
 
-type Log = [LogEntry]
-
--- 00:02:20,476
-parseTime :: String -> Parser LocalTime
-parseTime s = do
-    h <- count 2 digit
-    char ':'
-    m <- count 2 digit
-    char ':'
-    s <- count 2 digit
-    char ','
-    ms <- count 3 digit
-    return $ Parser { localDay = fromGregorian 1 1 1
-                    , localTimeOfDay = (read h) (read m) (read s) }
+instance Show SrtTime where
+    show (SrtTime h m s ms) = 
+        let pz = paddingZeros
+        in (pz 2 h) ++ ":" ++ (pz 2 m) ++ ":" ++ (pz 2 s) ++ "," ++ (pz 3 ms)
+    
+paddingZeros :: Int -> Int -> String
+paddingZeros n p = printf ("%0" ++ (show n) ++ "d") p
 
 main :: IO ()
-main = print $ parseOnly parseTime "00:02:20,476"
+main = putStr "Hello World"
