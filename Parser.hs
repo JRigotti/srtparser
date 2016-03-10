e05062e346c9b63c7e7beb288e7701425a72f899
Jean Rigotti <jean.michael.rigotti@sap.com>
2016-03-10 10:08:10 -0300

    Create data log entry format

--- Parser.hs
+++ Parser.hs
@@ -12,6 +12,11 @@ instance Show SrtTime where
     show (SrtTime h m s ms) = 
         let pz = paddingZeros
         in (pz 2 h) ++ ":" ++ (pz 2 m) ++ ":" ++ (pz 2 s) ++ "," ++ (pz 3 ms)
+
+data LogEntry = LogEntry { logNumber :: Int
+                         , start     :: SrtTime
+                         , end       :: SrtTime
+                         , message   :: String }
     
 paddingZeros :: Int -> Int -> String
 paddingZeros n p = printf ("%0" ++ (show n) ++ "d") p
