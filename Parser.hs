1c38e3bca0271e2c97ee1cb19aa33729bca9180f
Jean Rigotti <jean.m.rigotti@gmail.com>
2016-03-15 17:11:14 -0300

    Separate code by files

--- Parser.hs
+++ Parser.hs
@@ -1,31 +1,10 @@
+module Parser where
+
 import Text.ParserCombinators.ReadP
-import Text.Printf
-import Data.List (intercalate)
 import Control.Applicative
 import Data.Char (isPrint, isDigit)
+import Srt
 
--- Creating an specific dateformat
--- used in srt files
-data SrtTime = SrtTime { hours   :: Int
-                       , minutes :: Int
-                       , seconds :: Int
-                       , milli   :: Int }
-
-instance Show SrtTime where
-    show (SrtTime h m s ms) = 
-        let pz = paddingZeros
-        in (pz 2 h) ++ ":" ++ (pz 2 m) ++ ":" ++ (pz 2 s) ++ "," ++ (pz 3 ms)
-
-data LogEntry = LogEntry { logNumber :: Int
-                         , start     :: SrtTime
-                         , end       :: SrtTime
-                         , message   :: String }
-                         
-instance Show LogEntry where
-    show (LogEntry logn st end msg) = intercalate "\n" [(show logn), (show st) ++ " --> " ++ (show end), msg]
-    
-paddingZeros :: Int -> Int -> String
-paddingZeros n p = printf ("%0" ++ (show n) ++ "d") p
 
 digits :: ReadP Int
 digits = do
---
+++ Srt.hs
@@ -0,0 +1,28 @@
+module Srt (
+    SrtTime (SrtTime), 
+    LogEntry (LogEntry)
+) where
+
+import Data.List (intercalate)
+import Text.Printf (printf)
+
+data SrtTime = SrtTime { hours   :: Int
+                       , minutes :: Int
+                       , seconds :: Int
+                       , milli   :: Int }
+
+instance Show SrtTime where
+    show (SrtTime h m s ms) = 
+        let pz = paddingZeros
+        in (pz 2 h) ++ ":" ++ (pz 2 m) ++ ":" ++ (pz 2 s) ++ "," ++ (pz 3 ms)
+
+data LogEntry = LogEntry { logNumber :: Int
+                         , start     :: SrtTime
+                         , end       :: SrtTime
+                         , message   :: String }
+                         
+instance Show LogEntry where
+    show (LogEntry logn st end msg) = intercalate "\n" [(show logn), (show st) ++ " --> " ++ (show end), msg]
+    
+paddingZeros :: Int -> Int -> String
+paddingZeros n p = printf ("%0" ++ (show n) ++ "d") p
